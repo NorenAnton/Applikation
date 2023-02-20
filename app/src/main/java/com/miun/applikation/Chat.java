72c000f69cb8112bfa2826ef7cfaea8dfc66aff9
Gipe1900 <gipe1900@student.miun.se>
2023-02-20 15:14:39 +0100

    Fixed fillers java class for the fillList and fillChat, and added buttons to go from chat to log and log to chat

--- app/src/main/java/com/miun/applikation/Chat.java
+++ app/src/main/java/com/miun/applikation/Chat.java
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Chat extends AppCompatActivity{
+public class Chat extends AppCompatActivity {
 
     Fillers fillers = new Fillers();
     Button btn_goBack, btn_goToLog;
