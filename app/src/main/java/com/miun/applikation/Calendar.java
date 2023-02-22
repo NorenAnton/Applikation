9f791c84881c17d309cd8bf197e60793667c3930
Anton Norén <nati.noren@live.se>
2023-02-22 14:44:16 +0100

    Added calendar

---
+++ app/src/main/java/com/miun/applikation/Calendar.java
@@ -0,0 +1,37 @@
+package com.miun.applikation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class Calendar extends AppCompatActivity{
+
+    CalendarView kalender;
+    TextView test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar);
+
+        kalender = findViewById(R.id.calendarView);
+        test = findViewById(R.id.textView);
+
+        kalender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+             @Override
+             public void onSelectedDayChange(
+                     @NonNull CalendarView view,
+                     int year,
+                     int month,
+                     int dayOfMonth)
+             {
+                 String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
+                 test.setText(Date);
+             }
+        });
+    }
+}
