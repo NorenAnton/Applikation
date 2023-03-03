16a4eb51c336872efe8dfedb5fa2dab20d41c208
Englund <anton95@live.se>
2023-03-03 11:14:00 +0100

    Test med att retuerna data

--- app/src/main/java/com/miun/applikation/MainActivity.java
+++ app/src/main/java/com/miun/applikation/MainActivity.java
@@ -29,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.println("WAT");
+        //Log.d("NumberGenerated", "Function has generated zero.");
+
         // TESTAR klient, bara så ni vet!
         //retrofitClient client = setUpClient("https://jsonplaceholder.typicode.com/");
         // TODO: Varje person måste hämta sin egen IP wifi, ska göra en bat fil som sköter detta automatiskt
@@ -37,8 +40,22 @@ public class MainActivity extends AppCompatActivity {
 
         MessageModelPost newMessage =
                 new MessageModelPost(1, 2, "HEEEEJ", "fancystuff.png");
-        Call<MessageModelPost> caller = client.storeMessage(newMessage);
 
+        returnDataTest(client);
+
+        //  data = datareader.GET_data()
+        // view
+        // R
+
+        // layouts:
+        // main
+        // chat
+        // log
+        // kalander
+
+        // GET_data(appElement)
+        // user newUser
+        /*Call<MessageModelPost> caller = client.storeMessage(newMessage);
         caller.enqueue(new Callback<MessageModelPost>() {
             @Override
             public void onResponse(Call<MessageModelPost> call, Response<MessageModelPost> response) {
@@ -49,34 +66,16 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(response.errorBody());
                 System.out.println(response.code());
                 //System.out.println(response.body().getPersId());
+
+                // newUser = *lägg i data här*
             }
 
             @Override
             public void onFailure(Call<MessageModelPost> call, Throwable t) {
                 System.out.println("ERROR: " + t);
             }
-        });
-
-
-
-        /*Call<List<PersonTemp>> caller = client.getAllPersons();
-        caller.enqueue(new Callback<List<PersonTemp>>() {
-            @Override
-            public void onResponse(Call<List<PersonTemp>> call, Response<List<PersonTemp>> response) {
-                System.out.println("DATA! HURRA");
-
-                List<PersonTemp> responsData = response.body();
-                for (PersonTemp p : responsData) {
-                    System.out.println(p.getId());
-                    System.out.println(p.getFname());
-                    System.out.println("-----------------------------------------");
-                }
-            }
-            @Override
-            public void onFailure(Call<List<PersonTemp>> call, Throwable t) {
-                System.err.println("ERROR, ingen kontakt" + t);
-            }
         });*/
+
 
 
         btn_Chatt = findViewById(R.id.chat);
@@ -102,6 +101,27 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create()).build();
 
         return bob.create(retrofitClient.class);
+    }
+
+    private void returnDataTest(retrofitClient client) {
+        Call<List<PersonTemp>> caller = client.getAllPersons();
+        caller.enqueue(new Callback<List<PersonTemp>>() {
+            @Override
+            public void onResponse(Call<List<PersonTemp>> call, Response<List<PersonTemp>> response) {
+                System.out.println("DATA! HURRA");
+
+                List<PersonTemp> responsData = response.body();
+                for (PersonTemp p : responsData) {
+                    System.out.println(p.getId());
+                    System.out.println(p.getFname());
+                    System.out.println("-----------------------------------------");
+                }
+            }
+            @Override
+            public void onFailure(Call<List<PersonTemp>> call, Throwable t) {
+                System.err.println("ERROR, ingen kontakt" + t);
+            }
+        });
     }
 
 }
