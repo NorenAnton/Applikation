fb53ee78cdd26ec1d15e00efb28c6d17f400cf41
Vincent Johansson <vincent-johansson@hotmail.com>
2023-03-03 12:45:47 +0100

    Added ability to click on image and open it in a imageviewer

--- app/src/main/java/com/miun/applikation/chat/ChatAdapter.java
+++ app/src/main/java/com/miun/applikation/chat/ChatAdapter.java
@@ -1,6 +1,7 @@
 package com.miun.applikation.chat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,12 +25,9 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
     Context context;
     List<CurrentChat> chatter;
 
-    int test;
-
     public ChatAdapter(Context context, List<CurrentChat> chatter){
         this.context = context;
         this.chatter = chatter;
-        test = 0;
     }
 
     @NonNull
@@ -44,9 +42,15 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
         holder.tv_chatName.setText(chatter.get(position).getName());
         holder.tv_message.setText(chatter.get(position).getMessage());
         holder.tv_date.setText(chatter.get(position).getDate());
-        if (chatter.get(position).getImage() != null)
-            Glide.with(context).load(chatter.get(position).getImage().toString()).override(600,500).into(holder.iv_image);
-        ++test;
+        if (chatter.get(position).getImage() != null) {
+            Glide.with(context).load(chatter.get(position).getImage().toString()).override(600, 500).into(holder.iv_image);
+            holder.iv_image.setOnClickListener(view -> {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setDataAndType(chatter.get(holder.getAdapterPosition()).getImage(), "image/*");
+                context.startActivity(intent);
+            });
+        }
     }
 
     @Override
